14e78e17c5468432ae044a9dddac6f34f3b7dfb1
BangTran <bangtranluong195@gmail.com>
2023-03-19 12:54:30 +0100

    update chatapp

--- chatapp.py
+++ chatapp.py
@@ -3,72 +3,64 @@ import gradio as gr
 from pydub import AudioSegment
 
 
-openai.api_key = "add-your-api-key"
+openai.api_key = "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"
 
 message_history = []
 audio_history = []
 
-
-def predict(input):
+def predict(message):
     global message_history
 
-    message_history.append({"role": "user", "content": input})
+    message_history.append({"role": "user", "content": message})
 
+    # Send request and get response from chatGPT API
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=message_history
+        model="gpt-3.5-turbo",
+        messages=message_history
     )
-
+    
     reply_content = completion.choices[0].message.content
-    print(reply_content)
-
+    
     message_history.append({"role": "user", "content": reply_content.strip()})
 
-    response = [
-        (message_history[i]["content"], message_history[i + 1]["content"])
-        for i in range(0, len(message_history) - 1, 2)
-    ]
+    response = [(message_history[i]["content"], message_history[i+1]["content"]) for i in range(0, len(message_history) - 1, 2)]
 
     return response
-
 
 def transcribe(audio_file):
     global audio_history
 
+    # Convert the audio file to wav file
     audio = AudioSegment.from_file(audio_file)
-    audio_file_wav = "./tmp/" + "record" + str(len(audio_history)) + ".wav"
-
+    audio_file_wav ="./tmp/" +  "record" + str(len(audio_history)) + ".wav"
     audio.export(audio_file_wav, format="wav")
 
+    # Transcipt audio file to text by whisper API
     audio_file = open(audio_file_wav, "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    print("user", transcript["text"])
 
     audio_history.append({"role": "user", "content": transcript["text"]})
 
+    # Send request and get response from chatGPT API
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=audio_history
+        model="gpt-3.5-turbo",
+        messages=audio_history
     )
 
     reply_content = completion.choices[0].message.content
 
     audio_history.append({"role": "user", "content": reply_content.strip()})
 
-    response = [
-        (audio_history[i]["content"], audio_history[i + 1]["content"])
-        for i in range(0, len(audio_history) - 1, 2)
-    ]
+    response = [(audio_history[i]["content"], audio_history[i+1]["content"]) for i in range(0, len(audio_history) - 1, 2)]
 
     return response
-
-
+    
 with gr.Blocks(theme=gr.themes.Soft()) as demo:
     gr.Markdown("### Chatbot with Openai ChatGPT API and Whisper API!")
     with gr.Tab("Text"):
         chatbot = gr.Chatbot()
         with gr.Row():
-            txt = gr.Textbox(
-                show_label=False, placeholder="Type your message here"
-            ).style(container=False)
+            txt = gr.Textbox(show_label=False, placeholder="Type your message here").style(container=False)
             txt.submit(predict, txt, chatbot)
             txt.submit(None, None, txt, _js="() => {''}")
 
