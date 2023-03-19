9086a965354fb98ead9b74d244a6b769a7e2eb9e
BangTran <bangtranluong195@gmail.com>
2023-03-19 12:26:22 +0100

    upload chatbot

---
+++ README.md
@@ -0,0 +1,39 @@
+## Chatbot with OpenAI ChatGPT API and Whisper API
+This is a chatbot application that uses ChatGPT API with `gpt-3.5-turbo` and Speech-to-Text by Whisper API with `whisper-1` for natural language processing and understanding. The chatbot can communicate with users via a web interface created using Gradio.
+
+### Features
+
+- Natural Language Processing (NLP) powered by ChatGPT API and GPT-2.5-turbo for generating responses to user input
+- Speech-to-Text transcription using Whisper API for allowing users to speak to the chatbot via a microphone
+- User-friendly web interface created with Gradio, a Python library for building customizable UI components
+- Flexible customization options for Gradio UI components, including text inputs, voice inputs, and dropdown menus
+
+### Requirements
+- Python 
+- openai
+- gradio
+- ffmpeg (for audio processing)
+- A ChatGPT API key (sign up [here](https://auth0.openai.com/u/signup/identifier?state=hKFo2SAwZzlYR0pjYmpXNTZ4QTZYM2hISnZpVWM1OGRSclNLa6Fur3VuaXZlcnNhbC1sb2dpbqN0aWTZIEx5QzN1WTdSekxJRks1R0twRi1YajZkQ19rZ3RXR3ZJo2NpZNkgRFJpdnNubTJNdTQyVDNLT3BxZHR3QjNOWXZpSFl6d0Q) for a free account)
+
+### Installation
+1. Clone the repo `https://github.com/TranLuongBang/Chatbot-with-ChatGPT-API-Whisper-API`
+2. Navigate to the project directory: `cd Chatbot-with-ChatGPT-API-Whisper-API`
+3. Create environment and Install the requirements
+```python
+python -m venv .venv
+source .venv/bin/active
+pip install -r requirements.txt
+```
+Install ffmpeg for audio precessing
+```
+sudo apt update && sudo apt upgrade
+sudo apt install ffmpeg
+ffmpeg -version
+```
+
+### Usage
+1. Run the app: python chatapp.py
+
+2. Open a web browser and go to http://127.0.0.1:7860
+
+3. Type in a message or speak into the microphone to start a conversation with the chatbot
---
+++ chatapp.py
@@ -0,0 +1,73 @@
+import openai
+import gradio as gr
+from pydub import AudioSegment
+
+
+openai.api_key = "add-your-api-key"
+
+message_history = []
+audio_history = []
+
+def predict(input):
+    global message_history
+
+    message_history.append({"role": "user", "content": input})
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=message_history
+    )
+    
+    reply_content = completion.choices[0].message.content
+    print(reply_content)
+    
+    message_history.append({"role": "user", "content": reply_content.strip()})
+
+    response = [(message_history[i]["content"], message_history[i+1]["content"]) for i in range(0, len(message_history) - 1, 2)]
+
+    return response
+
+def transcribe(audio_file):
+    global audio_history
+
+    audio = AudioSegment.from_file(audio_file)
+    audio_file_wav ="./tmp/" +  "record" + str(len(audio_history)) + ".wav"
+
+    audio.export(audio_file_wav, format="wav")
+
+    audio_file = open(audio_file_wav, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    print("user", transcript["text"])
+
+    audio_history.append({"role": "user", "content": transcript["text"]})
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=audio_history
+    )
+
+    reply_content = completion.choices[0].message.content
+
+    audio_history.append({"role": "user", "content": reply_content.strip()})
+
+    response = [(audio_history[i]["content"], audio_history[i+1]["content"]) for i in range(0, len(audio_history) - 1, 2)]
+
+    return response
+    
+with gr.Blocks(theme=gr.themes.Soft()) as demo:
+    gr.Markdown("### Chatbot with Openai ChatGPT API and Whisper API!")
+    with gr.Tab("Text"):
+        chatbot = gr.Chatbot()
+        with gr.Row():
+            txt = gr.Textbox(show_label=False, placeholder="Type your message here").style(container=False)
+            txt.submit(predict, txt, chatbot)
+            txt.submit(None, None, txt, _js="() => {''}")
+
+    with gr.Tab("Audio"):
+        chatbot = gr.Chatbot()
+        with gr.Row():
+            audio = gr.Audio(source="microphone", type="filepath")
+            greet_btn = gr.Button("Send")
+            greet_btn.click(fn=transcribe, inputs=audio, outputs=chatbot)
+
+demo.launch()
